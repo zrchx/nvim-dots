fdabea73b64edaba1658da60485a7048787035d4
zrchx <epsilonrf0@gmail.com>
2023-04-20 18:56:29 -0500

    qwqjisadj

--- lua/config/heirconf.lua
+++ lua/config/heirconf.lua
@@ -14,18 +14,26 @@ local cond = require('heirline.conditions')
 
 -- ====================================
 -- Separator Module
-local separator = function (direction, thick, bgcolor, fgcolor)
-    -- Left
-    if direction == "left" and thick == "light" then
+local separator = function (direction, thick, sharp, bgcolor, fgcolor)
+    -- Left rounded
+    if direction == 'left' and thick == 'light' and sharp == nil then
       return { provider = ' ', hl = {bg = bgcolor, fg = fgcolor }}
-    elseif direction == "left" and thick == "heavy" then
+    elseif direction == 'left' and thick == 'heavy' and sharp == nil then
       return { provider = ' ', hl = {bg = bgcolor, fg = fgcolor }}
     end
-    -- Right
-    if direction == "right" and thick == "light"then
+    -- Right rounded
+    if direction == 'right' and thick == 'light' and sharp == nil then
       return { provider = ' ', hl = {bg = bgcolor, fg = fgcolor }}
-    elseif direction == "right" and thick == "heavy" then
+    elseif direction == 'right' and thick == 'heavy' and sharp == nil then
       return { provider = ' ', hl = {bg = bgcolor, fg = fgcolor }}
+    end
+    -- Left sharp
+    if direction == 'left' and thick == nil and sharp == 'true' then
+      return { provider = '', hl = {bg = bgcolor, fg = fgcolor}}
+    end
+    -- Right sharp
+    if direction == 'right' and thick == nil and sharp == 'true' then
+      return { provider = '', hl = {bg = bgcolor, fg = fgcolor}}
     end
 end
 -- ====================================
@@ -40,88 +48,81 @@ local vimodes = {
   static = {
     mode_names = {
       -- Normal Modes
-      n = "Normal",
-      no = "NormalO",
-      nov = "NormalO",
-      noV = "NormalO",
-      ["no\22"] = "Normal?",
-      niI = "NormalI",
-      niR = "NormalR",
-      niV = "NormalV",
-      nt = "NormalT",
-      ntT = "NormalT",
+      n = 'Normal',
+      no = 'NormalO',
+      nov = 'NormalO',
+      noV = 'NormalO',
+      ['no\22'] = 'Normal?',
+      niI = 'NormalI',
+      niR = 'NormalR',
+      niV = 'NormalV',
+      nt = 'NormalT',
+      ntT = 'NormalT',
       -- Visual Modes
-      v = "Visual",
-      vs = "VisualS",
-      V = "Visual_",
-      VS = "Visual_S",
-      ["\22"] = "Visual^",
-      ["\22s"] = "Visual^",
+      v = 'Visual',
+      vs = 'VisualS',
+      V = 'Visual_',
+      VS = 'Visual_S',
+      ['\22'] = 'Visual^',
+      ['\22s'] = 'Visual^',
       -- Select Modes
-      s = "Select",
-      S = "Select_",
-      ["\19"] = "Select^",
+      s = 'Select',
+      S = 'Select_',
+      ['\19'] = 'Select^',
       -- Insert Modes
-      i = "Insert",
-      ic = "InsertC",
-      ix = "InsertX",
+      i = 'Insert',
+      ic = 'InsertC',
+      ix = 'InsertX',
       -- Replace Modes
-      R = "Replace",
-      Rc = "ReplaceC",
-      Rx = "ReplaceX",
-      Rv = "ReplaceV",
-      Rvc = "ReplaceV",
-      Rvx = "ReplaceV",
+      R = 'Replace',
+      Rc = 'ReplaceC',
+      Rx = 'ReplaceX',
+      Rv = 'ReplaceV',
+      Rvc = 'ReplaceV',
+      Rvx = 'ReplaceV',
       -- Various Modes
-      r = "Hit",
-      rm = "More",
-      ["!"] = "Sc!",
-      ["r?"] = "Confirm",
-      c = "ComandLine",
-      t = "Term",
+      r = 'Hit',
+      rm = 'More',
+      ['!'] = 'Sc!',
+      ['r?'] = 'Confirm',
+      c = 'ComandLine',
+      t = 'Term',
     },
     -- Colorize modes
     mode_colors = {
-      n = "red" ,
-      i = "green",
-      v = "cyan",
-      V =  "cyan",
-      ["\22"] =  "cyan",
-      c =  "orange",
-      s =  "purple",
-      S =  "purple",
-      ["\19"] =  "purple",
-      R =  "orange",
-      r =  "orange",
-      ["!"] =  "red",
-      t =  "red",
+      n = 'red',
+      i = 'green',
+      v = 'cyan',
+      V =  'cyan',
+      ['\22'] =  'cyan',
+      c =  'orange',
+      s =  'purple',
+      S =  'purple',
+      ['\19'] =  'purple',
+      R =  'orange',
+      r =  'orange',
+      ['!'] =  'red',
+      t =  'red',
     }
   },
   -- Icon and text
   {
     provider = function (self)
-      return " %4("..self.mode_names[self.mode].."%) "
+      return ' %4('..self.mode_names[self.mode]..'%) '
     end,
     hl = function (self)
       local mode = self.mode:sub(1, 1)
-      return { bg = "bg_dark", fg = self.mode_colors[mode], bold = true }
+      return { bg = self.mode_colors[mode], fg = 'bg_dark', bold = true }
     end
   },
   {
-    provider = ' 󰘧 ',
+    provider = '',
     hl = function (self)
       local mode = self.mode:sub(1, 1)
-      return { bg = self.mode_colors[mode], fg = "bg_dark", bold = true }
+      return { bg = 'bg_dark', fg = self.mode_colors[mode], bold = true }
     end
   },
-  {
-    provider = '',
-    hl = function (self)
-      local mode = self.mode:sub(1, 1)
-      return { bg = "bg_dark", fg = self.mode_colors[mode], bold = true }
-    end
-  },
-  update = { "ModeChanged", pattern = "*:*" },
+  update = { 'ModeChanged', pattern = '*:*' },
 }
 -- ====================================
 
@@ -136,21 +137,21 @@ local files = {
   {
     init = function (self)
       local filename = self.filename
-      local extension = vim.fn.fnamemodify(filename, ":e")
+      local extension = vim.fn.fnamemodify(filename, ':e')
       self.icon, self.icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
     end,
     provider = function (self)
-      return self.icon and (self.icon .. " ")
+      return self.icon and (self.icon .. ' ')
     end,
     hl = function (self)
-      return { fg = self.icon_color }
+      return { bg = 'bg_dark', fg = self.icon_color }
     end
   },
   -- File Name
   {
     provider = function (self)
-      local filename = vim.fn.fnamemodify(self.filename, ":.")
-      if filename == "" then return " [Empty] " end
+      local filename = vim.fn.fnamemodify(self.filename, ':.')
+      if filename == '' then return ' [Empty] ' end
       if not cond.width_percent_below(#filename, 0.25) then
         filename = vim.fn.pathshorten(filename)
       end
@@ -158,9 +159,9 @@ local files = {
     end,
     hl = function ()
       if vim.bo.modified then
-        return { bg = "bg_dark", fg = "cyan", bold = true }
+        return { bg = 'bg_dark', fg = 'cyan', bold = true }
       else
-        return { bg = "bg_dark", fg = "white"}
+        return { bg = 'bg_dark', fg = 'white' }
       end
     end
   },
@@ -170,15 +171,15 @@ local files = {
       condition = function ()
         return vim.bo.modified
       end,
-      provider = " ",
-      hl = { bg = "bg_dark", fg = "green" }
+      provider = ' ',
+      hl = { bg = 'bg_dark', fg = 'green' }
     },
     {
       condition = function ()
         return not vim.bo.modifiable or vim.bo.readonly
       end,
-      provider = " ",
-      hl = { bg = "bg_dark", fg = "orange" }
+      provider = ' ',
+      hl = { bg = 'bg_dark', fg = 'orange' }
     },
   },
   -- Separator
@@ -197,22 +198,22 @@ local ruler = {
     separator('left', 'heavy', 'bg_dark', 'cyan')
   },
   { -- Lines
-    --provider = "  %L",
-    --hl = {'bg_dark', 'green'}
+    provider = '  %L',
+    hl = { bg = 'bg_dark', fg = 'green'}
   },
   { -- Separator
     separator('left', 'light', 'bg_dark', 'cyan')
   },
   { -- Current Line
-    --provider = " %l :  %2c",
-    --hl = {'bg_dark', 'yellow'}
+    provider = '  %l :  %c ',
+    hl = { bg = 'bg_dark', fg = 'yellow'}
   },
   { -- Separator
     separator('right', 'light', 'bg_dark', 'cyan'),
   },
   { -- Percentaje
-    --provider = "󰠞 %P ",
-    --hl = {'bg_dark', 'green'}
+    provider = '󰠞 %P ',
+    hl = { bg = 'bg_dark', fg = 'green'}
   },
   { -- Separator
     separator('right', 'heavy', 'bg_dark', 'cyan')
@@ -227,7 +228,7 @@ local scrollbar ={
     local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
     return string.rep(self.sbar[i], 2)
   end,
-  hl = { fg = "blue", bg = "bg_dark" },
+  hl = { bg = 'bg_dark', fg = 'blue'},
 }
 -- ====================================
 
@@ -236,24 +237,18 @@ local scrollbar ={
 -- LSP
 local lsp_active = {
   condition = cond.lsp_attached,
-  update = { "LspAttach", "LspDetach" },
-  provider = function ()
-    local name = {}
-    for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-      table.insert(name, server.name)
-    end
-    return "   LSP ".. table.concat(name, " ") .. "  "
-  end,
-  hl = { fg = "green", bold = true }
+  update = { 'LspAttach', 'LspDetach' },
+  provider = '   LSP  ',
+  hl = { fg = 'green', bold = true }
 }
 
 local diagnostic = {
   condition = cond.has_diagnostics,
   static = {
-    error_icon = "  ",
-    warn_icon = "  ",
-    hint_icon = "  ",
-    info_icon = "  ",
+    error_icon = '  ',
+    warn_icon = '  ',
+    hint_icon = '  ',
+    info_icon = '  ',
   },
   init = function(self)
     self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
@@ -261,38 +256,34 @@ local diagnostic = {
     self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
     self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
   end,
-  update = { "DiagnosticChanged", "BufEnter" },
+  update = { 'DiagnosticChanged', 'BufEnter' },
   {
-    provider = " ",
-    hl = { fg = "yellow"}
   },
   {
     provider = function(self)
-      return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+      return self.errors > 0 and (self.error_icon .. self.errors .. ' ')
     end,
-    hl = { fg = "red" },
+    hl = { fg = 'red' },
   },
   {
     provider = function(self)
-      return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+      return self.warnings > 0 and (self.warn_icon .. self.warnings .. ' ')
     end,
-    hl = { fg = "yellow" },
+    hl = { fg = 'yellow' },
   },
   {
     provider = function(self)
-      return self.info > 0 and (self.info_icon .. self.info .. " ")
+      return self.info > 0 and (self.info_icon .. self.info .. ' ')
     end,
-    hl = { fg = "blue" },
+    hl = { fg = 'blue' },
   },
   {
     provider = function(self)
       return self.hints > 0 and (self.hint_icon .. self.hints)
     end,
-    hl = { fg = "cyan" },
+    hl = { fg = 'cyan' },
   },
   {
-    provider = " ",
-    hl = { fg = "yellow"}
   },
 }
 -- ====================================
@@ -309,33 +300,33 @@ local git = {
   -- Branch
   {
     provider = function(self)
-      return " " .. self.status_dict.head
+      return ' ' .. self.status_dict.head
     end,
-    hl = { fg = "gray", bold = true }
+    hl = { fg = 'gray', bold = true }
   },
   -- Add git
   {
     provider = function(self)
       local count = self.status_dict.added or 0
-      return count > 0 and (" " .. count)
+      return count > 0 and (' ' .. count)
     end,
-    hl = { fg = "cyan" },
+    hl = { fg = 'cyan' },
   },
   -- Removed git
   {
     provider = function(self)
       local count = self.status_dict.removed or 0
-      return count > 0 and (" " .. count)
+      return count > 0 and (' ' .. count)
     end,
-    hl = { fg = "red" },
+    hl = { fg = 'red' },
   },
   -- Changed git
   {
     provider = function(self)
       local count = self.status_dict.changed or 0
-      return count > 0 and (" " .. count)
+      return count > 0 and (' ' .. count)
     end,
-    hl = { fg = "orange" },
+    hl = { fg = 'orange' },
   },
 }
 -- ====================================
